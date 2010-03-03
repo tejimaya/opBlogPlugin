1991730a739ff3da559b96b44d928047a333e5a2
Masato Nagasawa <nagasawa@tejimaya.com>
2010-03-03 13:42:19 +0900

    fixed deleted a image, it was not able to display. (fixes #648)

--- apps/pc_frontend/templates/_partsBlogListBox.php
+++ apps/pc_frontend/templates/_partsBlogListBox.php
@@ -2,7 +2,6 @@
 <?php foreach($options['blogRssCacheList'] as $blogRssCache): ?>
 <li>
 <span class="date"><?php echo op_format_date($blogRssCache->getDate(), 'XShortDateJa') ?></span>
-<?php echo image_tag('articleList_maker.gif', array('alt' => '')) ?> 
 <?php
 echo link_to(op_truncate($blogRssCache->getTitle(), 30), $blogRssCache->getLink());
 ?>
